702afaf3d1f77f8b9c2397e135a6323c74b91e60
MahdiGreen75 <qarimahdi217@gmail.comF>
2023-08-19 00:22:15 +0600

    full site done

--- scripts/area-calculator.js
+++ scripts/area-calculator.js
@@ -9,15 +9,14 @@
  * function.
  */
 
-
-function clickHandlerProducer(inputId1, inputId2, whereToShowId, formulaConstant) {
+function clickHandlerProducer(inputId1, inputId2, whereToShowId, formulaConstant, identity) {
 
     function getNumFromInpField(inputId) {
         const inputField = document.getElementById(inputId);
         const inputFieldValueString = inputField.value;
         const inputFieldValueNum = parseFloat(inputFieldValueString);
         /*** Validation of the inputs ***/
-        if (inputFieldValueNum < 0 || isNaN(inputFieldValueNum)) { 
+        if (inputFieldValueNum < 0 || isNaN(inputFieldValueNum)) {
             alert("Invalid Input");
             return;
         }
@@ -31,23 +30,38 @@ function clickHandlerProducer(inputId1, inputId2, whereToShowId, formulaConstant
         textElement.innerText = fixedDecimalValue;
     }
 
+    
+
     return function () {
         const x = getNumFromInpField(inputId1);
         const y = getNumFromInpField(inputId2);
+        
         /*** Validation of the inputs ***/
-        if(!x || !y)  {
+        if (!x || !y) {
             return;
         }
         const area = formulaConstant * x * y;
         setAreaValueToTextElement(whereToShowId, area);
+
+        function areaCalculationEntry() {
+            const areaEntry = document.getElementById("areaEntry");
+            let count = areaEntry.childElementCount;
+            const p = document.createElement("p");
+            p.classList.add("flex", "flex-row", "justify-between", "items-center", "text-xs", "mb-1");
+            p.innerHTML = `<span>${count}. ${identity}</span> 
+                           <span class="flex flex-row items-center justify-end">${area}cm<sup>2</sup></span>
+                           <button  class="rounded px-4 py-2 font-bold bg-sky-600 hover:bg-sky-700 text-white">Convert to m<sup>2</sup></button>`;
+            areaEntry.appendChild(p);
+        }
+        areaCalculationEntry();
     }
 }
 
 /***************************************************************************/
 
-let calculateTriangleArea = clickHandlerProducer("baseTriangle", "heightTriangle", "triangleAreaShow", 0.5);
-let calculateReatangleArea = clickHandlerProducer("rectangleWidth", "rectangleLength", "rectangleAreaShow", 1);
-let calculateParallelogramArea = clickHandlerProducer("parallelogramBase", "parallelogramHeight", "prallelogramAreaShow", 1);
-let calculateRhombusArea = clickHandlerProducer("rhombusDiagonalOne", "rhombusDiagonalTwo", "rhombusAreaShow", 0.5);
-let calculatePentagonArea = clickHandlerProducer("pentagonP", "pentagonB", "pentagonAreaShow", 1);
-let calculateEllipseArea = clickHandlerProducer("ellipseA", "ellipseB", "ellipseAreaShow", 3.14);
+let calculateTriangleArea = clickHandlerProducer("baseTriangle", "heightTriangle", "triangleAreaShow", 0.5, "Tringle");
+let calculateReatangleArea = clickHandlerProducer("rectangleWidth", "rectangleLength", "rectangleAreaShow", 1, "Rectangle");
+let calculateParallelogramArea = clickHandlerProducer("parallelogramBase", "parallelogramHeight", "prallelogramAreaShow", 1, "Paralleogram");
+let calculateRhombusArea = clickHandlerProducer("rhombusDiagonalOne", "rhombusDiagonalTwo", "rhombusAreaShow", 0.5, "Rhombus");
+let calculatePentagonArea = clickHandlerProducer("pentagonP", "pentagonB", "pentagonAreaShow", 1, "Pentagon");
+let calculateEllipseArea = clickHandlerProducer("ellipseA", "ellipseB", "ellipseAreaShow", 3.14, "Ellipse");
