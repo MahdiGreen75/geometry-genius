39c21352bc14f6fafa6681e3db03c66b9b8222d4
MahdiGreen75 <qarimahdi217@gmail.comF>
2023-08-18 17:07:29 +0600

    all the cards are done

--- scripts/area-calculator.js
+++ scripts/area-calculator.js
@@ -1,33 +1,43 @@
-function getNumFromInpField(inputId) {
-    const inputField = document.getElementById(inputId);
-    const inputFieldValueString = inputField.value;
-    const inputFieldValueNum = parseFloat(inputFieldValueString);
-    return inputFieldValueNum;
+
+/**************************************************************************/
+/**********
+ * User manual of "clickHandlerProducer" function
+ * formulaConstant = firstCosntant X secondParameter X thirdParameter]
+ * if the formula coantains no constants.such as area = a* b  . Then,
+ * the firstConstant should be 1 forcefully. rest of the parameter will
+ * be as it is. secondParameter and thirdParameter will be added by this
+ * function.
+ */
+
+
+function clickHandlerProducer(inputId1, inputId2, whereToShowId, formulaConstant) {
+
+    function getNumFromInpField(inputId) {
+        const inputField = document.getElementById(inputId);
+        const inputFieldValueString = inputField.value;
+        const inputFieldValueNum = parseFloat(inputFieldValueString);
+        return inputFieldValueNum;
+    }
+    
+    function setAreaValueToTextElement(textElementId, value) {
+        const textElement = document.getElementById(textElementId);
+        const fixedDecimalValue = value.toFixed(2);
+        textElement.innerText = fixedDecimalValue;
+    }
+
+    return function() {
+        const x = getNumFromInpField(inputId1);
+        const y = getNumFromInpField(inputId2);
+        const area = formulaConstant * x * y;
+        setAreaValueToTextElement(whereToShowId, area);
+    }
 }
 
-function setAreaValueToTextElement(textElementId, value) {
-    const textElement = document.getElementById(textElementId);
-    const fixedDecimalValue = value.toFixed(2);
-    textElement.innerText = fixedDecimalValue;
-}
+/***************************************************************************/
 
-function calculateTriangleArea() {
-   const b = getNumFromInpField("baseTriangle");
-   const h = getNumFromInpField("heightTriangle");
-   const area = 0.5 * b * h;
-   setAreaValueToTextElement("triangleAreaShow", area);
-}
-
-function calculateReatangleArea() {
-    const w = getNumFromInpField("rectangleWidth");
-    const h = getNumFromInpField("rectangleLength");
-    const area = w * h;
-    setAreaValueToTextElement("rectangleAreaShow", area);
-}
-
-function calculateParallelogramArea() {
-    const b = getNumFromInpField("parallelogramBase");
-    const h = getNumFromInpField("parallelogramHeight");
-    const area = b * h;
-    setAreaValueToTextElement("prallelogramAreaShow", area);
-}
+let calculateTriangleArea = clickHandlerProducer("baseTriangle", "heightTriangle","triangleAreaShow", 0.5);
+let calculateReatangleArea = clickHandlerProducer("rectangleWidth", "rectangleLength", "rectangleAreaShow", 1);
+let calculateParallelogramArea = clickHandlerProducer("parallelogramBase", "parallelogramHeight", "prallelogramAreaShow", 1);
+let calculateRhombusArea = clickHandlerProducer("rhombusDiagonalOne", "rhombusDiagonalTwo", "rhombusAreaShow", 0.5);
+let calculatePentagonArea = clickHandlerProducer("pentagonP", "pentagonB", "pentagonAreaShow", 1);
+let calculateEllipseArea = clickHandlerProducer("ellipseA", "ellipseB", "ellipseAreaShow", 3.14);
